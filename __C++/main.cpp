3acc87d96159ec989c7bfc7daa7407e3ef28d2d9
Jaewon Lee <joyjwlee@gmail.com>
2021-08-03 06:07:01 -0700

    v to s

--- __C++/main.cpp
+++ __C++/main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
-#include <queue>
 #include <string>
 #include <vector>
+#include <queue>
+#include <set>
 
 using namespace std;
 
 int x, y;
-vector<vector<char>> v;
+vector<vector<char> > v;
 
 void init() {
     cout << "Enter number of rows:" << endl;
@@ -18,12 +19,40 @@ void init() {
         v[i].resize(y);
 
     cout << "Enter the board: " << endl;
-    cout << "(B for block, T for target, space for empty, and * for wall" << endl;
+    cout << "(B for block, T for target, . for empty, and * for wall" << endl;
     for (int i = 0; i < x; i++)
         for (int j = 0; j < y; j++)
             cin >> v[i][j];
 }
 
+string vToS(vector<vector<char> > vec) {
+    string str = "";
+    for (int i = 0; i < x; i++)
+        for (int j = 0; j < y; j++)
+            str += vec[i][j];
+    return str;
+}
+
+vector<vector<char> > sToV(string str) {
+    //
+}
+
 int main(void) {
     init();
+    cout << "\nBoard:\n";
+    for (int i = 0; i < x; i++) {
+        for (int j = 0; j < y; j++)
+            cout << v[i][j] << " ";
+        cout << endl;
+    }
+    cout << "\n"
+         << vToS(v);
 }
+
+/*
+*...*
+.....
+B.T.B
+.....
+*...*
+*/
